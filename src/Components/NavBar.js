6335dcfd626e8eaf563592518c0c181b3a670e4b
Akhil Mishra <akhilmishra184@gmail.com>
2023-08-30 00:26:34 +0530

    added logo icon with css

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,16 @@
 import React from "react";
+import { ReactComponent as Logo } from "../assets/siteLogo.svg";
 
 const NavBar = () => {
-  return <div className="navbar-container">NavBar</div>;
+  return (
+    <div className="navbar-container">
+      <div className="logo-container">
+        <div className="logo-icon">
+          <Logo />
+        </div>
+        <div className="logo-text">E-COMM</div>
+      </div>
+    </div>
+  );
 };
 export default NavBar;
