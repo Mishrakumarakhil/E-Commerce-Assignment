54ab78bf7de394d17c5aa58038896b1556696365
Akhil Mishra <akhilmishra184@gmail.com>
2023-08-29 23:58:13 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import MainContainer from './Components/MainContainer';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-   
+   <NavBar/>
+   <MainContainer/>
     </div>
   );
 }
